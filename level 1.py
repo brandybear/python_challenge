e6e0b93fab290f25d96263e63ae15e521ed18e6a
Brandy Bannister <blbannister@gmail.com>
2013-01-27 17:41:34 -0600

    second commit - after lvl 1

--- level 1.py
+++ level 1.py
@@ -1,2 +1,25 @@
 __author__ = 'Brandy'
-print "Hello World!"
+import string
+d = {"a":"c", "b":"d", "c":"e", "d":"f", "e":"g", "f":"h", "g":"i", "h":"j", "i":"k", "j":"l", "k":"m", "l":"n", "m":"o", "n":"p", "o":"q", "p":"r", "q":"s", "r":"t", "s":"u", "t":"v", "u":"w", "v":"x", "w":"y", "x":"z", "y":"a", "z":"b"}
+phrase = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+url = "http://www.pythonchallenge.com/pc/def/map.html"
+for x in phrase:
+    if x == " ":
+        print " "
+    elif x == ",":
+        print ","
+    elif x == ".":
+        print "."
+    elif x == "'":
+        print "'"
+    elif x == "(":
+        print "("
+    elif x == ")":
+        print ")"
+    else:
+        print d[x]
+
+frm = "abcdefghijklmnopqrstuvwxyz"
+to = "cdefghijklmnopqrstuvwxyzab"
+translation = string.maketrans(frm, to)
+print url.translate(translation)
